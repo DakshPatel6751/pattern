86d5846e5bd12a8abf43b4f737c022b62668987d
Daksh Patel <157096650+DakshPatel6751@users.noreply.github.com>
2024-05-30 16:09:14 +0530

    Add files via upload

---
+++ pattrn/3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+	printf(" \n\n\n*\t*\t*\t*\t*\t*");
+	printf(" \n\n\n*\t\t\t\t*");
+	printf(" \n\n\n*\t\t\t*");
+	printf(" \n\n\n*\t\t*");
+	printf(" \n\n\n*\t*");
+	printf(" \n\n\n*");
+	getch();
+}
---
+++ pattrn/4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	printf("\t\t\t\t\t\t*");
+	printf("\n\t\t\t\t\t*\t\t*");
+	printf("\n\t\t\t\t*\t\t*\t\t*");
+	printf("\n\t\t\t*\t\t*\t\t*\t\t*");
+	printf("\n\t\t*\t\t*\t\t*\t\t*\t\t*");
+	printf("\n\t*\t\t*\t\t*\t\t*\t\t*\t\t*");
+}
